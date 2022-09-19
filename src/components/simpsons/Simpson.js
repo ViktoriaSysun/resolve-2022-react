5eaa45711d50da3c983850811e7a178082cf811e
ViktoriaSysun <vikasysun18@gmail.com>
2022-09-19 19:07:19 +0300

    hw1

---
+++ src/components/simpsons/Simpson.js
@@ -0,0 +1,14 @@
+function Simpson(props) {
+    console.log(props);
+    const {simpson} = props;
+
+    return (
+        <div>
+            <h3>{simpson.name} -- {simpson.age}</h3>
+            <p>{simpson.surname} -- {simpson.info}</p>
+            <img src={simpson.photo} alt={simpson.name}/>
+        </div>
+    );
+}
+
+export default Simpson;
